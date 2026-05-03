6ccaa579c399ece0a5543f165ffa2562fc92c033
sambhavgulani <sambhavnonugulani@gmail.com>
2026-05-04 00:26:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-//function scrollToPortfolio() {
-    //alert("Portfolio section coming soon!");
-//}
+const toggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Smooth scroll
+function scrollToWork() {
+    document.getElementById('short').scrollIntoView({ behavior: 'smooth' });
+}
+
+// Disable right click globally (extra protection)
+document.addEventListener('contextmenu', event => event.preventDefault());
